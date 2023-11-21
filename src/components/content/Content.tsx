1ba5b02abb64f77dc4c7938d45b78b343317bf20
oligregz <gregorysak90@gmail.com>
2023-11-21 20:32:45 -0300

    refact/feat: change type files and add contens scope

---
+++ src/components/content/Content.tsx
@@ -0,0 +1,13 @@
+const Content = () => {
+
+  return (
+    <div className='container content'>
+      <div className="pic">
+        <img src='https://media.licdn.com/dms/image/D4D03AQFqtKFZy8h_ug/profile-displayphoto-shrink_400_400/0/1694700071635?e=1706140800&v=beta&t=m1g8WRcZfomASsmA13FJVAwcXupcte9kQzRyS7h-Dz8' alt="my picture" />
+      </div>
+      <div className="title"></div>
+    </div>
+  )
+}
+
+export default Content
