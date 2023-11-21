140f9f5083799675fe7bbf0a0161353ee869be33
oligregz <gregorysak90@gmail.com>
2023-11-21 20:54:22 -0300

    style/feat: add background color and text title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Navbar from './components/navbar'
 import Content from './components/content'
 import Sidebar from './components/sidebar'
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="container">
--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,11 +1,18 @@
-const Content = () => {
+import './style/style.css'
 
+const Content = () => {
   return (
     <div className='container content'>
       <div className="pic">
         <img src='https://media.licdn.com/dms/image/D4D03AQFqtKFZy8h_ug/profile-displayphoto-shrink_400_400/0/1694700071635?e=1706140800&v=beta&t=m1g8WRcZfomASsmA13FJVAwcXupcte9kQzRyS7h-Dz8' alt="my picture" />
       </div>
-      <div className="title"></div>
+      <div className="title">
+        <h1>
+          Hey <br/>
+          My name is Gregory <br/>
+          I'm a FullStack Developer <br/>
+        </h1>
+      </div>
     </div>
   )
 }
